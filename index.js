2921ccd45375c385b5e7014797ce40af8b7e7b19
Rupesh155 <rudhg35@gmail.com>
2024-10-22 15:03:18 +0530

    new

---
+++ index.js
@@ -0,0 +1,101 @@
+// console.log('hello');
+
+// int a =5
+// var vs let vs consttt
+// var a
+// console.log(a);
+
+// Number   5, 5.5
+// String 
+// Boolean true,false
+// undefined
+// null
+
+
+// 'c'  "c"
+
+
+// var a=5
+// var b='c'
+
+
+// var a=5
+// var b=10
+// console.log(a+b);
+// var a=5
+// var a=5
+// var a=10
+// console.log(a);
+
+// let b=10
+// b=10000
+// console.log(b);
+// const c=10
+//  c=100
+//  console.log(c);
+// =  vs  == vs ===
+// console.log(0=='');
+// let score=100
+// let to =score==100
+// console.log(to);
+
+// let std={
+//     id:1,
+//     name:"lalit",
+//     age:16,
+//     isPass:false
+
+// }
+// console.log(std.name);
+
+// let arr=[1,2,3,'hello',false]
+
+
+// console.log(arr);
+// for(let i in arr){
+//     console.log(i);
+// }
+// for(let i of arr){
+//     console.log(i);
+// }
+
+
+
+// sum(5,5)
+// function sum(a,b){
+//     console.log('hello');
+    
+// }
+
+
+
+
+// sum()
+
+// const sum=()   =>  {
+//     console.log('hehehe');
+    
+
+// }
+
+// var a
+// console.log(
+//     a
+// );
+
+
+console.log(a);
+var a=10
+
+
+
+
+
+
+
+ 
+
+
+
+
+
